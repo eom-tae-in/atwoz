10f2925e60283d1a9b84a2232ee24b6546ce4546
devholic22 <hyunjoon.tech@gmail.com>
2024-03-17 15:31:50 +0900

    refactor: MemberMissionNotFoundException 메시지 수정
    - MemberMissionNotFoundException 예외 메시지 수정

--- src/main/java/com/atwoz/mission/exception/membermission/exceptions/MemberMissionNotFoundException.java
+++ src/main/java/com/atwoz/mission/exception/membermission/exceptions/MemberMissionNotFoundException.java
@@ -3,6 +3,6 @@ package com.atwoz.mission.exception.membermission.exceptions;
 public class MemberMissionNotFoundException extends RuntimeException {
 
     public MemberMissionNotFoundException() {
-        super("미션을 가지고 있지 않습니다.");
+        super("미션이 회원 미션 목록에 존재하지 않습니다.");
     }
 }
