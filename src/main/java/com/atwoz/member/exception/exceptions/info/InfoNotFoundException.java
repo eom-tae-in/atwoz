779715525232199e4818761209478a768a480f03
eom-tae-in <eti0728@daum.net>
2024-04-02 13:00:37 +0900

    remove: 불필요한 클래스 제거

--- src/main/java/com/atwoz/member/exception/exceptions/info/InfoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.atwoz.member.exception.exceptions.info;
-
-public class InfoNotFoundException extends RuntimeException {
-
-    public InfoNotFoundException() {
-        super("회원 정보가 저장되지 않았습니다.");
-    }
-}
--- src/main/java/com/atwoz/member/exception/exceptions/info/profile/ProfileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.atwoz.member.exception.exceptions.info.profile;
-
-public class ProfileNotFoundException extends RuntimeException {
-
-    public ProfileNotFoundException() {
-        super("Profile이 저장되지 않았습니다.");
-    }
-}
