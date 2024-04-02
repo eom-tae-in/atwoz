711f297af9e0c9b7d4181340591d8abce08c0a49
eom-tae-in <eti0728@daum.net>
2024-04-02 14:03:45 +0900

    test: MemberProfileInfoFixture 구현

---
+++ src/test/java/com/atwoz/member/fixture/domain/member/MemberProfileInfoFixture.java
@@ -0,0 +1,19 @@
+package com.atwoz.member.fixture.domain.member;
+
+import com.atwoz.member.application.member.dto.ProfileInitializeRequest;
+import com.atwoz.member.application.member.dto.ProfileUpdateRequest;
+import com.atwoz.member.domain.member.dto.MemberProfileInfo;
+import com.atwoz.member.infrastructure.member.physical_profile.FakeYearManager;
+
+public class MemberProfileInfoFixture {
+
+    public static MemberProfileInfo 회원_프로필_정보_생성(final ProfileInitializeRequest profileInitializeRequest) {
+
+        return MemberProfileInfo.createWith(profileInitializeRequest, new FakeYearManager());
+    }
+
+    public static MemberProfileInfo 회원_프로필_정보_생성(final ProfileUpdateRequest profileUpdateRequest) {
+
+        return MemberProfileInfo.createWith(profileUpdateRequest, new FakeYearManager());
+    }
+}
