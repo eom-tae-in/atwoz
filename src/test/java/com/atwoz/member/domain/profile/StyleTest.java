088c59cc1b76d21718b322583326538f7e865eab
eom-tae-in <eti0728@daum.net>
2024-04-02 14:08:09 +0900

    test: Style 클래스를 테스트하는 StyleTest 생성

---
+++ src/test/java/com/atwoz/member/domain/profile/StyleTest.java
@@ -0,0 +1,73 @@
+package com.atwoz.member.domain.profile;
+
+import com.atwoz.member.domain.member.profile.Style;
+import com.atwoz.member.exception.exceptions.member.profile.InvalidStyleException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class StyleTest {
+
+    @DisplayName("스타일_코드_값으로_Style을_찾는다")
+    @Nested
+    class StyleSearch {
+
+        @Test
+        void 스타일_코드_값이_유효하면_Style을_찾아_반환한다() {
+            // given
+            String validStyleCode = "C001";
+
+            // when
+            Style style = Style.findByCode(validStyleCode);
+
+            // then
+            assertThat(style.getCode()).isEqualTo(validStyleCode);
+        }
+
+        @Test
+        void 스타일_코드_값이_유효하지_않으면_예외가_발생한다() {
+            // given
+            String invalidStyleCode = "invalidStyleCode";
+
+            // when & then
+            assertThatThrownBy(() -> Style.findByCode(invalidStyleCode))
+                    .isInstanceOf(InvalidStyleException.class);
+        }
+    }
+
+    @DisplayName("스타일_코드가_유효하면_true_유효하지_않으면_false를_반환한다")
+    @Nested
+    class StyleCodeValidation {
+
+        @Test
+        void 코드가_유효한_값이면_true를_반환한다() {
+            // given
+            String validStyleCode = "C001";
+
+            // when
+            boolean result = Style.isValidCode(validStyleCode);
+
+            // then
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        void 코드가_유효한_값이_아니면_false를_반환한다() {
+            // given
+            String invalidStyleCode = "invalidStyleCode";
+
+            // when
+            boolean result = Style.isValidCode(invalidStyleCode);
+
+            // then
+            assertThat(result).isFalse();
+        }
+    }
+}
