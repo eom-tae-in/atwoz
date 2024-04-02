6fe760c5931b7dfdb3654b0d00757576dad64fe4
eom-tae-in <eti0728@daum.net>
2024-04-02 13:33:50 +0900

    feat: 회원의 등급 정보를 가지는 MemberGrade 구현

---
+++ src/main/java/com/atwoz/member/domain/member/MemberGrade.java
@@ -0,0 +1,13 @@
+package com.atwoz.member.domain.member;
+
+public enum MemberGrade {
+    DIAMOND("다이아"),
+    GOLD("골드"),
+    SILVER("실버");
+
+    private final String grade;
+
+    MemberGrade(final String grade) {
+        this.grade = grade;
+    }
+}
