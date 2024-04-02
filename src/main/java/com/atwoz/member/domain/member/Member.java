0259628405d026b9dd6cbb0533b8a866b26336f2
eom-tae-in <eti0728@daum.net>
2024-04-02 13:19:50 +0900

    refactor: Member 요구사항 변경으로 인한 코드 수정
    - SoftDelete 적용
    - email 필드 제거
    - recommender, phoneNumber, MemberGrade, MemberStatus 필드 추가
    - memberProfile과 일대일 매핑

--- src/main/java/com/atwoz/member/domain/member/Member.java
+++ src/main/java/com/atwoz/member/domain/member/Member.java
@@ -1,62 +1,106 @@
 package com.atwoz.member.domain.member;
 
-import com.atwoz.global.domain.BaseEntity;
+import com.atwoz.global.domain.SoftDeleteBaseEntity;
+import com.atwoz.member.domain.member.dto.MemberProfileInfo;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import static com.atwoz.member.domain.member.MemberGrade.SILVER;
+import static com.atwoz.member.domain.member.MemberRole.MEMBER;
+import static com.atwoz.member.domain.member.MemberStatus.ACTIVE;
 
 @Getter
 @Builder
 @EqualsAndHashCode(of = "id", callSuper = false)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE member SET deleted_at = current_timestamp WHERE id = ?")
 @Entity
-public class Member extends BaseEntity {
+public class Member extends SoftDeleteBaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
     private String nickname;
+
+    private String recommender;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @OneToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private MemberProfile memberProfile;
 
     @Enumerated(value = EnumType.STRING)
     @Column(nullable = false)
     private MemberRole memberRole;
 
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private MemberGrade memberGrade;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private MemberStatus memberStatus;
+
     public boolean isAdmin() {
         return this.memberRole.isAdministrator();
     }
 
-    public static Member createDefaultRole(final String email,
-                                           final NicknameGenerator nicknameGenerator) {
+    public static Member createWithOAuth(final String phoneNumber) {
+
         return Member.builder()
-                .email(email)
-                .nickname(nicknameGenerator.createRandomNickname())
-                .memberRole(MemberRole.MEMBER)
+                .phoneNumber(phoneNumber)
+                .memberProfile(MemberProfile.createWith("남성"))
+                .memberGrade(SILVER)
+                .memberStatus(ACTIVE)
+                .memberRole(MEMBER)
                 .build();
     }
 
-    public static Member createWithOAuthLogin(final String email,
-                                              final String nickname) {
+    public static Member createWithPass(final String gender,
+                                        final String phoneNumber) {
 
         return Member.builder()
-                .email(email)
-                .nickname(nickname)
-                .memberRole(MemberRole.MEMBER)
+                .phoneNumber(phoneNumber)
+                .memberProfile(MemberProfile.createWith(gender))
+                .memberGrade(SILVER)
+                .memberStatus(ACTIVE)
+                .memberRole(MEMBER)
                 .build();
+    }
+
+    public void initializeWith(final String nickname, final String recommender,
+                               final MemberProfileInfo memberProfileInfo) {
+        this.nickname = nickname;
+        initializeRecommender(recommender);
+        memberProfile.change(memberProfileInfo);
+    }
+
+    private void initializeRecommender(final String recommender) {
+        if (recommender != null && !recommender.isBlank()) {
+            this.recommender = recommender;
+        }
+    }
+
+    public void updateWith(final String nickname, final MemberProfileInfo memberProfileInfo) {
+        this.nickname = nickname;
+        memberProfile.change(memberProfileInfo);
     }
 }
