9ea1516341bdd4fee7b9a0c46c8d5b70bfca9d73
eom-tae-in <eti0728@daum.net>
2024-04-02 13:44:20 +0900

    feat: 회원의 상태 정보를 가지는 MemberStatus 구현

---
+++ src/main/java/com/atwoz/member/domain/member/MemberStatus.java
@@ -0,0 +1,14 @@
+package com.atwoz.member.domain.member;
+
+public enum MemberStatus {
+    ACTIVE("활동중"),
+    HUMAN("휴먼"),
+    TEMPORARY_SUSPENSION("임시정지"),
+    PERMANENT_BAN("영구정지");
+
+    private final String status;
+
+    MemberStatus(final String status) {
+        this.status = status;
+    }
+}
