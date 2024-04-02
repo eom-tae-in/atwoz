381e46791c3d6530bbb2fd9f6fdaa35f631b38aa
eom-tae-in <eti0728@daum.net>
2024-04-02 14:07:49 +0900

    test: Religion 클래스를 테스트하는 ReligionTest 생성

---
+++ src/test/java/com/atwoz/member/domain/profile/ReligionTest.java
@@ -0,0 +1,44 @@
+package com.atwoz.member.domain.profile;
+
+import com.atwoz.member.domain.member.profile.Religion;
+import com.atwoz.member.exception.exceptions.member.profile.InvalidReligionException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ReligionTest {
+
+    @DisplayName("종교_정보로_Religion을_찾는다")
+    @Nested
+    class ReligionSearch {
+
+        @Test
+        void 종교_정보가_유효하면_Religion을_찾아_반환한다() {
+            // given
+            String validReligion = "기독교";
+
+            // when
+            Religion religion = Religion.findByName(validReligion);
+
+            // then
+            assertThat(religion.getName()).isEqualTo(validReligion);
+        }
+
+        @Test
+        void 종교_정보가_유효하지_않으면_예외가_발생한다() {
+            // given
+            String invalidReligion = "invalidReligion";
+
+            // when & then
+            assertThatThrownBy(() -> Religion.findByName(invalidReligion))
+                    .isInstanceOf(InvalidReligionException.class);
+        }
+    }
+}
