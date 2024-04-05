7ef4f876e40c49495bcc9381a3945ff0b5b1d7ea
devholic22 <hyunjoon.tech@gmail.com>
2024-04-05 15:12:36 +0900

    test: memberMission 서비스 테스트 추가 작성
    - 바뀐 도메인에 대한 서비스 테스트 추가 작성
    - MethodSource를 활용하여 중복 테스트 공통 처리 진행

--- src/test/java/com/atwoz/mission/application/membermission/MemberMissionsServiceTest.java
+++ src/test/java/com/atwoz/mission/application/membermission/MemberMissionsServiceTest.java
@@ -6,6 +6,8 @@ import com.atwoz.mission.domain.membermission.MemberMissions;
 import com.atwoz.mission.domain.membermission.MemberMissionsRepository;
 import com.atwoz.mission.domain.mission.Mission;
 import com.atwoz.mission.domain.mission.MissionRepository;
+import com.atwoz.mission.exception.membermission.exceptions.AlreadyChallengeMissionExistedException;
+import com.atwoz.mission.exception.membermission.exceptions.AlreadyDailyMissionExistedLimitException;
 import com.atwoz.mission.exception.membermission.exceptions.MemberMissionAlreadyRewardedException;
 import com.atwoz.mission.exception.membermission.exceptions.MemberMissionNotFoundException;
 import com.atwoz.mission.exception.mission.exceptions.MissionNotFoundException;
@@ -16,11 +18,20 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
 import static com.atwoz.mission.fixture.MemberMissionFixture.멤버_미션_생성_완료_보상_수령_안함_데일리;
 import static com.atwoz.mission.fixture.MemberMissionFixture.멤버_미션_생성_완료_보상_수령_완료_데일리;
+import static com.atwoz.mission.fixture.MemberMissionFixture.멤버_미션_생성_완료_보상_안함_데일리_id_시간있음;
 import static com.atwoz.mission.fixture.MemberMissionsFixture.멤버_미션들_생성;
 import static com.atwoz.mission.fixture.MissionFixture.미션_생성_리워드_100_데일리_공개;
+import static com.atwoz.mission.fixture.MissionFixture.미션_생성_리워드_100_데일리_공개_id없음;
+import static com.atwoz.mission.fixture.MissionFixture.미션_생성_리워드_200_챌린지_id없음;
+import static com.atwoz.mission.fixture.MissionFixture.미션_생성_리워드_200_챌린지_공개;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -40,18 +51,31 @@ class MemberMissionsServiceTest {
         memberMissionsService = new MemberMissionsService(memberMissionsRepository, missionRepository);
     }
 
-    @Test
-    void 회원_미션_목록에_미션을_등록한다() {
+    @ParameterizedTest(name = "성별이 [{0}]인 경우")
+    @EnumSource(Gender.class)
+    void 회원_미션_목록에_챌린지_미션을_등록한다(final Gender gender) {
         // given
         MemberMissions memberMissions = 멤버_미션들_생성();
         memberMissionsRepository.save(memberMissions);
-
-        Gender memberGender = Gender.MALE;
-        Mission mission = 미션_생성_리워드_100_데일리_공개();
-        Mission saveMission = missionRepository.save(mission);
+        Mission mission = missionRepository.save(미션_생성_리워드_200_챌린지_id없음());
 
         // when
-        memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), memberGender, saveMission.getId());
+        memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), gender, mission.getId());
+
+        // then
+        assertThat(memberMissions.getMemberMissions()).isNotEmpty();
+    }
+
+    @ParameterizedTest(name = "성별이 [{0}]인 경우")
+    @EnumSource(Gender.class)
+    void 회원_미션_목록에_데일리_미션을_등록한다(final Gender gender) {
+        // given
+        MemberMissions memberMissions = 멤버_미션들_생성();
+        memberMissionsRepository.save(memberMissions);
+        Mission mission = missionRepository.save(미션_생성_리워드_100_데일리_공개_id없음());
+
+        // when
+        memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), gender, mission.getId());
 
         // then
         assertThat(memberMissions.getMemberMissions()).isNotEmpty();
@@ -90,6 +114,44 @@ class MemberMissionsServiceTest {
             // when & then
             assertThatThrownBy(() -> memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), memberGender, missionId))
                     .isInstanceOf(MissionNotFoundException.class);
+        }
+
+        @ParameterizedTest(name = "성별이 [{0}]인 경우")
+        @EnumSource(Gender.class)
+        void 회원_미션_목록에_같은_챌린지_미션이_또_들어가면_예외가_발생한다(final Gender gender) {
+            // given
+            MemberMissions memberMissions = 멤버_미션들_생성();
+            memberMissionsRepository.save(memberMissions);
+            memberMissions.addClearedMission(gender, MemberMission.createDefault(미션_생성_리워드_200_챌린지_공개()));
+            Mission anotherMission = missionRepository.save(미션_생성_리워드_200_챌린지_id없음());
+
+            // when & then
+            assertThatThrownBy(() -> memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), gender, anotherMission.getId()))
+                    .isInstanceOf(AlreadyChallengeMissionExistedException.class);
+        }
+
+        @ParameterizedTest(name = "성별이 [{0}]일 때 같은 날 데일리 미션을 {1}개 등록할 경우")
+        @MethodSource("genderAndLimitData")
+        void 회원은_성별에_따라_같은날_같은_데일리_미션을_진행할_수_있는_최대_횟수가_다르다(final Gender gender, final Long max) {
+            // given
+            MemberMissions memberMissions = 멤버_미션들_생성();
+            memberMissionsRepository.save(memberMissions);
+            missionRepository.save(미션_생성_리워드_100_데일리_공개());
+
+            for (long id = 1; id <= max; id++) {
+                memberMissions.addClearedMission(gender, 멤버_미션_생성_완료_보상_안함_데일리_id_시간있음(id));
+            }
+
+            // when & then
+            assertThatThrownBy(() -> memberMissionsService.addClearedMemberMission(memberMissions.getMemberId(), gender, max + 1))
+                    .isInstanceOf(AlreadyDailyMissionExistedLimitException.class);
+        }
+
+        static Stream<Arguments> genderAndLimitData() {
+            return Stream.of(
+                    Arguments.of(Gender.MALE, 2L),
+                    Arguments.of(Gender.FEMALE, 3L)
+            );
         }
 
         @Test
