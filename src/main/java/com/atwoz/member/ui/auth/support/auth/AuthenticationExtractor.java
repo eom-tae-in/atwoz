d0fa9f1dc56e392f08641720d428f1904f4bafe6
eom-tae-in <eti0728@daum.net>
2024-04-14 13:33:44 +0900

    refactor: 응답에서 토큰 추출하는 기능 추가 및 JsonMapper를 사용하기 위해 @Component 추가

--- src/main/java/com/atwoz/member/ui/auth/support/auth/AuthenticationExtractor.java
+++ src/main/java/com/atwoz/member/ui/auth/support/auth/AuthenticationExtractor.java
@@ -1,19 +1,29 @@
 package com.atwoz.member.ui.auth.support.auth;
 
+import com.atwoz.member.domain.auth.JsonMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
+@RequiredArgsConstructor
+@Component
 public class AuthenticationExtractor {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String BEARER = "Bearer";
     private static final String HEADER_SPLIT_DELIMITER = " ";
+    private static final String TOKEN = "token";
     private static final int TOKEN_TYPE_INDEX = 0;
     private static final int TOKEN_VALUE_INDEX = 1;
     private static final int VALID_HEADER_SPLIT_LENGTH = 2;
 
-    public static Optional<String> extract(final HttpServletRequest request) {
+    private final JsonMapper jsonMapper;
+
+    public Optional<String> extractFromRequest(final HttpServletRequest request) {
         String header = request.getHeader(AUTHORIZATION_HEADER);
 
         if (!StringUtils.hasText(header)) {
@@ -23,7 +33,7 @@ public class AuthenticationExtractor {
         return extractFromHeader(header.split(HEADER_SPLIT_DELIMITER));
     }
 
-    public static Optional<String> extractFromHeader(final String[] headerParts) {
+    public Optional<String> extractFromHeader(final String[] headerParts) {
         if (headerParts.length == VALID_HEADER_SPLIT_LENGTH &&
                 headerParts[TOKEN_TYPE_INDEX].equals(BEARER)) {
             return Optional.ofNullable(headerParts[TOKEN_VALUE_INDEX]);
@@ -31,4 +41,16 @@ public class AuthenticationExtractor {
 
         return Optional.empty();
     }
+
+    public String extractFromResponse(final HttpServletResponse response) {
+        ContentCachingResponseWrapper responseWrapper = (ContentCachingResponseWrapper) response;
+        String responseBody = getResponseBody(responseWrapper);
+
+        return jsonMapper.getValueByKey(responseBody, TOKEN);
+    }
+
+    private String getResponseBody(final ContentCachingResponseWrapper responseWrapper) {
+        return new String(responseWrapper.getContentAsByteArray());
+    }
 }
+
