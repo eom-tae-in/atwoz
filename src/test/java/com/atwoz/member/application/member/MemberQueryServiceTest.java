acaf08c8f0a9636abdbab67b010276267710cf24
eom-tae-in <eti0728@daum.net>
2024-05-02 00:09:29 +0900

    test: MemberQueryServiceTest 생성

---
+++ src/test/java/com/atwoz/member/application/member/MemberQueryServiceTest.java
@@ -0,0 +1,78 @@
+package com.atwoz.member.application.member;
+
+import com.atwoz.member.application.member.dto.MemberNicknameRequest;
+import com.atwoz.member.domain.member.Member;
+import com.atwoz.member.domain.member.MemberRepository;
+import com.atwoz.member.exception.exceptions.member.MemberNicknameAlreadyExistedException;
+import com.atwoz.member.infrastructure.member.MemberFakeRepository;
+import com.atwoz.member.infrastructure.member.dto.MemberResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.atwoz.member.fixture.HobbiesResponseFixture.취미_응답서_요청;
+import static com.atwoz.member.fixture.MemberFixture.일반_유저_생성;
+import static com.atwoz.member.fixture.MemberProfileResponseFixture.회원_프로필_응답서_요청;
+import static com.atwoz.member.fixture.StylesResponseFixture.스타일_응답서_요청;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class MemberQueryServiceTest {
+
+    private MemberQueryService memberQueryService;
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setup() {
+        memberRepository = new MemberFakeRepository();
+        memberQueryService = new MemberQueryService(memberRepository);
+    }
+
+    @Nested
+    class 닉네임_중복_확인 {
+
+        @Test
+        void 중복된_닉네임이_존재하는지_않으면_예외가_발생하지_않는다() {
+            // given
+            MemberNicknameRequest memberNicknameRequest = new MemberNicknameRequest("uniqueNickname");
+
+            // when & then
+            assertDoesNotThrow(() -> memberQueryService.checkMemberExists(memberNicknameRequest));
+        }
+
+        @Test
+        void 중복된_닉네임이_존재하면_예외가_발생한다() {
+            // given
+            memberRepository.save(일반_유저_생성());
+            MemberNicknameRequest memberNicknameRequest = new MemberNicknameRequest("nickname");
+
+            // when & then
+            assertThatThrownBy(() -> memberQueryService.checkMemberExists(memberNicknameRequest))
+                    .isInstanceOf(MemberNicknameAlreadyExistedException.class);
+        }
+    }
+
+    @Test
+    void 회원을_조회한다() {
+        // given
+        Member member = memberRepository.save(일반_유저_생성());
+
+        // when
+        MemberResponse memberResponse = memberQueryService.findMember(member.getId());
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(memberResponse.memberProfileResponse()).usingRecursiveComparison()
+                    .isEqualTo(회원_프로필_응답서_요청(member));
+            softly.assertThat(memberResponse.hobbiesResponse()).usingRecursiveComparison()
+                    .isEqualTo(취미_응답서_요청(member));
+            softly.assertThat(memberResponse.stylesResponse()).usingRecursiveComparison()
+                    .isEqualTo(스타일_응답서_요청(member));
+        });
+    }
+}
