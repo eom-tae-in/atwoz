7b32cc2e123f32887c66ce01bf46d49f3197063d
eom-tae-in <eti0728@daum.net>
2024-05-02 14:50:10 +0900

    refactor: 메서드 로직 수정

--- src/main/java/com/atwoz/member/application/member/MemberQueryService.java
+++ src/main/java/com/atwoz/member/application/member/MemberQueryService.java
@@ -1,6 +1,5 @@
 package com.atwoz.member.application.member;
 
-import com.atwoz.member.application.member.dto.MemberNicknameRequest;
 import com.atwoz.member.domain.member.MemberRepository;
 import com.atwoz.member.exception.exceptions.member.MemberNicknameAlreadyExistedException;
 import com.atwoz.member.infrastructure.member.dto.MemberResponse;
@@ -15,11 +14,7 @@ public class MemberQueryService {
 
     private final MemberRepository memberRepository;
 
-    public void checkMemberExists(final MemberNicknameRequest memberNicknameRequest) {
-        validateNicknameIsUnique(memberNicknameRequest.nickname());
-    }
-
-    private void validateNicknameIsUnique(final String nickname) {
+    public void checkMemberExists(final String nickname) {
         if (memberRepository.existsByNickname(nickname)) {
             throw new MemberNicknameAlreadyExistedException();
         }
