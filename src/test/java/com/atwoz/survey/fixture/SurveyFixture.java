27824f83795bf4f6358b02cbd4183c25bd18edc5
devholic22 <hyunjoon.tech@gmail.com>
2024-05-06 00:30:17 +0900

    test: SurveyJpaRepositoryTest 작성
    - SurveyJpaRepository 테스트 작성

---
+++ src/test/java/com/atwoz/survey/fixture/SurveyFixture.java
@@ -0,0 +1,10 @@
+package com.atwoz.survey.fixture;
+
+import com.atwoz.survey.domain.Survey;
+
+public class SurveyFixture {
+
+    public static Survey 설문_필수_질문_과목_두개씩() {
+        return Survey.createWith(SurveyCreateRequestFixture.설문_필수_질문_과목_두개씩());
+    }
+}
---
+++ src/test/java/com/atwoz/survey/infrastructure/SurveyJpaRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.atwoz.survey.infrastructure;
+
+import com.atwoz.survey.domain.Survey;
+import com.atwoz.survey.fixture.SurveyFixture;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+@DataJpaTest
+public class SurveyJpaRepositoryTest {
+
+    @Autowired
+    private SurveyJpaRepository surveyJpaRepository;
+
+    @Test
+    void 설문_과목_등록() {
+        // given
+        Survey survey = SurveyFixture.설문_필수_질문_과목_두개씩();
+
+        // when
+        Survey saveSurvey = surveyJpaRepository.save(survey);
+
+        // then
+        assertThat(saveSurvey).usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(survey);
+    }
+}
