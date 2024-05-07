9faf66e90437bb94ec73bf0984dd71fb0e670d83
devholic22 <hyunjoon.tech@gmail.com>
2024-05-07 14:40:24 +0900

    test: 답변 중복 등록 예외 테스트 작성
    - SurveyServiceTest에서의 답변 중복 등록 예외 테스트 작성

--- src/test/java/com/atwoz/survey/application/survey/SurveyServiceTest.java
+++ src/test/java/com/atwoz/survey/application/survey/SurveyServiceTest.java
@@ -1,7 +1,10 @@
 package com.atwoz.survey.application.survey;
 
+import com.atwoz.survey.application.survey.dto.SurveyAnswerCreateRequest;
 import com.atwoz.survey.application.survey.dto.SurveyCreateRequest;
+import com.atwoz.survey.application.survey.dto.SurveyQuestionCreateRequest;
 import com.atwoz.survey.domain.survey.SurveyRepository;
+import com.atwoz.survey.exception.survey.exceptions.SurveyAnswerDuplicatedException;
 import com.atwoz.survey.exception.survey.exceptions.SurveyAnswerNumberRangeException;
 import com.atwoz.survey.exception.survey.exceptions.SurveyNameAlreadyExistException;
 import com.atwoz.survey.exception.survey.exceptions.SurveyQuestionDuplicatedException;
@@ -11,10 +14,15 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static com.atwoz.survey.fixture.SurveyCreateRequestFixture.연애고사_필수_과목_질문_중복;
+import java.util.List;
+import java.util.stream.Stream;
 import static com.atwoz.survey.fixture.SurveyCreateRequestFixture.연애고사_필수_과목_질문_두개씩_생성_요청;
 import static com.atwoz.survey.fixture.SurveyCreateRequestFixture.연애고사_필수_과목_질문_번호_음수;
+import static com.atwoz.survey.fixture.SurveyCreateRequestFixture.연애고사_필수_과목_질문_중복;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -66,6 +74,30 @@ class SurveyServiceTest {
             // when & then
             assertThatThrownBy(() -> surveyService.addSurvey(request))
                     .isInstanceOf(SurveyQuestionDuplicatedException.class);
+        }
+
+        @MethodSource("invalidSurveyAnswerRequests")
+        @ParameterizedTest
+        void 번호나_내용이_같은_답변은_생성할_수_없다(final int answerNumber1, final String answer1, final int answerNumber2, final String answer2) {
+            // given
+            SurveyCreateRequest request = new SurveyCreateRequest("과목", true, List.of(
+                    new SurveyQuestionCreateRequest("질문", List.of(
+                            SurveyAnswerCreateRequest.of(answerNumber1, answer1),
+                            SurveyAnswerCreateRequest.of(answerNumber2, answer2)
+                    ))
+            ));
+
+            // when & then
+            assertThatThrownBy(() -> surveyService.addSurvey(request))
+                    .isInstanceOf(SurveyAnswerDuplicatedException.class);
+        }
+
+        static Stream<Arguments> invalidSurveyAnswerRequests() {
+            return Stream.of(
+                    Arguments.arguments(1, "답1", 1, "답2"),
+                    Arguments.arguments(1, "답1", 2, "답1"),
+                    Arguments.arguments(1, "답1", 1, "답1")
+            );
         }
 
         @Test
