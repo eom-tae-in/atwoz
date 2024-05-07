3e20b4bdfec1b18938077ef37957543d10657ca9
devholic22 <hyunjoon.tech@gmail.com>
2024-05-07 12:06:54 +0900

    test: SurveyJpaRepositoryTest 추가 작성
    - SurveyJpaRepository 테스트 추가 작성

--- src/test/java/com/atwoz/survey/fixture/SurveyCreateRequestFixture.java
+++ src/test/java/com/atwoz/survey/fixture/SurveyCreateRequestFixture.java
@@ -20,4 +20,17 @@ public class SurveyCreateRequestFixture {
                 ))
         ));
     }
+
+    public static SurveyCreateRequest 연애고사_선택_질문_과목_두개씩_생성_요청() {
+        return new SurveyCreateRequest("선택 과목 제목", false, List.of(
+                new SurveyQuestionCreateRequest("질문 내용1", List.of(
+                        SurveyAnswerCreateRequest.of(1, "답변1"),
+                        SurveyAnswerCreateRequest.of(2, "답변2")
+                )),
+                new SurveyQuestionCreateRequest("질문 내용2", List.of(
+                        SurveyAnswerCreateRequest.of(1, "답변1"),
+                        SurveyAnswerCreateRequest.of(2, "답변2")
+                ))
+        ));
+    }
 }
--- src/test/java/com/atwoz/survey/infrastructure/survey/SurveyJpaRepositoryTest.java
+++ src/test/java/com/atwoz/survey/infrastructure/survey/SurveyJpaRepositoryTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+import static com.atwoz.survey.fixture.SurveyFixture.연애고사_선택_질문_과목_두개씩;
 import static com.atwoz.survey.fixture.SurveyFixture.연애고사_필수_질문_과목_두개씩;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
@@ -30,5 +34,53 @@ public class SurveyJpaRepositoryTest {
         assertThat(saveSurvey).usingRecursiveComparison()
                 .ignoringFields("id")
                 .isEqualTo(survey);
+    }
+
+    @Test
+    void 연애고사_과목_존재여부_검사() {
+        // given
+        Survey survey = 연애고사_필수_질문_과목_두개씩();
+        surveyJpaRepository.save(survey);
+
+        // when
+        boolean find = surveyJpaRepository.existsByName(survey.getName());
+
+        // then
+        assertThat(find).isTrue();
+    }
+
+    @Test
+    void 연애고사_id로_과목_조회() {
+        // given
+        Survey survey = 연애고사_필수_질문_과목_두개씩();
+        Survey savedSurvey = surveyJpaRepository.save(survey);
+
+        // when
+        Optional<Survey> searchSurvey = surveyJpaRepository.findById(savedSurvey.getId());
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(searchSurvey).isPresent();
+            softly.assertThat(searchSurvey.get()).isEqualTo(savedSurvey);
+        });
+    }
+
+    @Test
+    void 필수_연애고사_조회() {
+        // given
+        Survey survey = 연애고사_필수_질문_과목_두개씩();
+        Survey notRequiredSurvey = 연애고사_선택_질문_과목_두개씩();
+        surveyJpaRepository.save(survey);
+        surveyJpaRepository.save(notRequiredSurvey);
+
+        // when
+        List<Survey> requiredSurveys = surveyJpaRepository.findAllRequiredSurveys();
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(requiredSurveys.size()).isEqualTo(1);
+            softly.assertThat(requiredSurveys.contains(survey)).isTrue();
+            softly.assertThat(requiredSurveys.contains(notRequiredSurvey)).isFalse();
+        });
     }
 }
