81d8368b7c568b344bc956f39f6c8c884ca4fccf
devholic22 <hyunjoon.tech@gmail.com>
2024-05-13 14:48:25 +0900

    refactor: valid message 추가
    - 연애고사 응시 제출 dto valid message 추가

--- src/main/java/com/atwoz/survey/application/membersurvey/dto/SurveyQuestionSubmitRequest.java
+++ src/main/java/com/atwoz/survey/application/membersurvey/dto/SurveyQuestionSubmitRequest.java
@@ -1,7 +1,11 @@
 package com.atwoz.survey.application.membersurvey.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public record SurveyQuestionSubmitRequest(
+        @NotNull(message = "응시할 연애고사 질문 id가 작성되어야 합니다.")
         Long questionId,
+        @NotNull(message = "응시할 연애고사 답변 번호가 작성되어야 합니다.")
         Integer answerNumber
 ) {
 }
--- src/main/java/com/atwoz/survey/application/membersurvey/dto/SurveySubmitRequest.java
+++ src/main/java/com/atwoz/survey/application/membersurvey/dto/SurveySubmitRequest.java
@@ -1,9 +1,13 @@
 package com.atwoz.survey.application.membersurvey.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record SurveySubmitRequest(
+        @NotNull(message = "응시할 연애고사 과목 id가 작성되어야 합니다.")
         Long surveyId,
+        @NotNull(message = "응시할 연애고사 질문이 작성되어야 합니다.")
         List<SurveyQuestionSubmitRequest> questions
 ) {
 }
