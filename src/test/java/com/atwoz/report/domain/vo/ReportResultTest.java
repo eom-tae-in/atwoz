15dec6e3345733dd250aa8f5221b7a06cd35aa5e
eom-tae-in <eti0728@daum.net>
2024-06-14 00:51:54 +0900

    test: ReportResult 테스트 작성

---
+++ src/test/java/com/atwoz/report/domain/vo/ReportResultTest.java
@@ -0,0 +1,43 @@
+package com.atwoz.report.domain.vo;
+
+import com.atwoz.report.exception.exceptions.InvalidReportResultException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.atwoz.report.domain.vo.ReportResult.BAN;
+import static com.atwoz.report.domain.vo.ReportResult.findByName;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ReportResultTest {
+
+    @Nested
+    class ReportResult_조회 {
+
+        @Test
+        void 신고_결과_정보가_유효하지_않으면_예외가_밠생한다() {
+            //  given
+            String invalidReportResult = "invalid report result";
+
+            // when
+            assertThatThrownBy(() -> findByName(invalidReportResult))
+                    .isInstanceOf(InvalidReportResultException.class);
+        }
+
+        @Test
+        void 신고_결과_정보가_유효하면_ReportResult을_찾아_반환한다() {
+            // given
+            String validReportResult = BAN.getName();
+
+            // When
+            ReportResult foundReportResult = findByName(validReportResult);
+
+            // then
+            assertThat(foundReportResult.getName()).isEqualTo(validReportResult);
+        }
+    }
+}
