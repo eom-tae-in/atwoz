3ed25628da8024180b617c2075d28cc21df179da
devholic22 <hyunjoon.tech@gmail.com>
2024-07-13 11:52:14 +0900

    refactor: AllArgsConstructor 추가
    - AlertMessage AllArgsConstructor 추가

--- src/main/java/com/atwoz/alert/domain/vo/AlertMessage.java
+++ src/main/java/com/atwoz/alert/domain/vo/AlertMessage.java
@@ -2,21 +2,18 @@ package com.atwoz.alert.domain.vo;
 
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Embeddable
 public class AlertMessage {
 
     private String title;
     private String body;
-
-    private AlertMessage(final String title, final String body) {
-        this.title = title;
-        this.body = body;
-    }
 
     public static AlertMessage createWith(final String title, final String body) {
         return new AlertMessage(title, body);
