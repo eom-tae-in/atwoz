3baa3fd92d3d72e9db0224bf6254fbbc85e395e9
eom-tae-in <eti0728@daum.net>
2024-07-16 22:11:21 +0900

    refactor: 개행 추가

--- src/main/java/com/atwoz/member/application/selfintro/dto/SelfIntroCreateRequest.java
+++ src/main/java/com/atwoz/member/application/selfintro/dto/SelfIntroCreateRequest.java
@@ -3,6 +3,7 @@ package com.atwoz.member.application.selfintro.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record SelfIntroCreateRequest(
+
         @NotBlank(message = "소개글을 입력해주세요")
         String content
 ) {
