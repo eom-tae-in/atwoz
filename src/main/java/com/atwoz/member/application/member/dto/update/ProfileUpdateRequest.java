4e0f68dbcec0851019d16b41eb35ede2c9356a6e
eom-tae-in <eti0728@daum.net>
2024-08-11 14:45:03 +0900

    feat: 프로필 관련 dto 생성 및 경로 변경

--- src/main/java/com/atwoz/member/application/member/dto/update/ProfileUpdateRequest.java
+++ src/main/java/com/atwoz/member/application/member/dto/update/ProfileUpdateRequest.java
@@ -1,15 +1,23 @@
-package com.atwoz.member.application.member.dto;
+package com.atwoz.member.application.member.dto.update;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 
-public record ProfileInitializeRequest(
-        @NotNull(message = "출생년도를 작성해주세요.")
-        Integer birthYear,
+public record ProfileUpdateRequest(
+        @Valid
+        @NotNull(message = "신체 프로필 정보를 입력해주세요.")
+        PhysicalProfileUpdateRequest physicalProfileUpdateRequest,
 
-        @NotNull(message = "키를 입력해주세요.")
-        Integer height,
+        @Valid
+        @NotNull(message = "취미 정보를 입력해주세요.")
+        HobbiesUpdateRequest hobbiesUpdateRequest,
+
+        @Valid
+        @NotNull(message = "스타일 정보를 입력해주세요.")
+        StylesUpdateRequest stylesUpdateRequest,
 
         @NotBlank(message = "거주하고 있는 광역시/도를 입력해주세요.")
         String city,
@@ -33,14 +41,16 @@ public record ProfileInitializeRequest(
         String religion,
 
         @NotBlank(message = "mbti를 입력해주세요.")
-        String mbti,
-
-        @Valid
-        @NotNull(message = "취미 정보를 입력해주세요.")
-        HobbiesRequest hobbiesRequest,
-
-        @Valid
-        @NotNull(message = "스타일 정보를 입력해주세요.")
-        StylesRequest stylesRequest
+        String mbti
 ) {
+
+    @JsonIgnore
+    public List<String> getHobbyCodes() {
+        return hobbiesUpdateRequest.hobbyCodes();
+    }
+
+    @JsonIgnore
+    public List<String> getStyleCodes() {
+        return stylesUpdateRequest.styleCodes();
+    }
 }
