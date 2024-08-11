14db2d3aeba0197016a665e6cd722d90609321b9
eom-tae-in <eti0728@daum.net>
2024-08-11 14:58:22 +0900

    feat: 요구사항 해당하는 조회 기능 추가

--- src/main/java/com/atwoz/member/application/member/MemberQueryService.java
+++ src/main/java/com/atwoz/member/application/member/MemberQueryService.java
@@ -1,8 +1,16 @@
 package com.atwoz.member.application.member;
 
+import com.atwoz.member.application.member.dto.ProfileFilterRequest;
 import com.atwoz.member.domain.member.MemberRepository;
+import com.atwoz.member.domain.member.profile.Hobby;
+import com.atwoz.member.domain.member.profile.HobbyRepository;
 import com.atwoz.member.exception.exceptions.member.MemberNicknameAlreadyExistedException;
+import com.atwoz.member.exception.exceptions.member.profile.hobby.HobbyNotFoundException;
+import com.atwoz.member.infrastructure.member.dto.InternalProfileFilterRequest;
 import com.atwoz.member.infrastructure.member.dto.MemberResponse;
+import com.atwoz.member.infrastructure.member.dto.ProfileResponse;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberQueryService {
 
     private final MemberRepository memberRepository;
+    private final HobbyRepository hobbyRepository;
 
     public void checkMemberExists(final String nickname) {
         if (memberRepository.existsByNickname(nickname)) {
@@ -22,5 +31,66 @@ public class MemberQueryService {
 
     public MemberResponse findMember(final Long memberId) {
         return memberRepository.findMemberWithId(memberId);
+    }
+
+    public List<ProfileResponse> findTodayProfiles(final ProfileFilterRequest profileFilterRequest,
+                                                   final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+        return memberRepository.findTodayProfiles(InternalProfileFilterRequest.from(profileFilterRequest), memberId);
+    }
+
+    public ProfileResponse findProfileByPopularity(final ProfileFilterRequest profileFilterRequest,
+                                                   final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findProfileByPopularity(InternalProfileFilterRequest.from(profileFilterRequest),
+                memberId);
+    }
+
+    public ProfileResponse findProfileByTodayVisit(final ProfileFilterRequest profileFilterRequest,
+                                                   final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findProfileByTodayVisit(InternalProfileFilterRequest.from(profileFilterRequest),
+                memberId);
+    }
+
+    public ProfileResponse findNearbyProfile(final ProfileFilterRequest profileFilterRequest,
+                                             final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findNearbyProfile(InternalProfileFilterRequest.from(profileFilterRequest), memberId);
+    }
+
+    public ProfileResponse findRecentProfile(final ProfileFilterRequest profileFilterRequest,
+                                             final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findRecentProfile(InternalProfileFilterRequest.from(profileFilterRequest), memberId);
+    }
+
+    public ProfileResponse findProfileByReligion(final ProfileFilterRequest profileFilterRequest,
+                                                 final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findProfileByReligion(InternalProfileFilterRequest.from(profileFilterRequest),
+                memberId);
+    }
+
+    public ProfileResponse findProfileByHobbies(final ProfileFilterRequest profileFilterRequest,
+                                                final Long memberId) {
+        validateHobbyCode(profileFilterRequest.hobbyCode());
+
+        return memberRepository.findProfileByHobbies(InternalProfileFilterRequest.from(profileFilterRequest), memberId);
+    }
+
+    private void validateHobbyCode(final String hobbyCode) {
+        if (hobbyCode == null || hobbyCode.isEmpty()) {
+            return;
+        }
+        Optional<Hobby> foundHobby = hobbyRepository.findHobbyByCode(hobbyCode);
+        if (foundHobby.isEmpty()) {
+            throw new HobbyNotFoundException();
+        }
     }
 }
--- src/main/java/com/atwoz/member/domain/member/MemberRepository.java
+++ src/main/java/com/atwoz/member/domain/member/MemberRepository.java
@@ -1,6 +1,9 @@
 package com.atwoz.member.domain.member;
 
+import com.atwoz.member.infrastructure.member.dto.InternalProfileFilterRequest;
 import com.atwoz.member.infrastructure.member.dto.MemberResponse;
+import com.atwoz.member.infrastructure.member.dto.ProfileResponse;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
@@ -12,6 +15,20 @@ public interface MemberRepository {
     Optional<Member> findByNickname(String nickname);
 
     MemberResponse findMemberWithId(Long id);
+
+    List<ProfileResponse> findTodayProfiles(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findProfileByPopularity(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findProfileByTodayVisit(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findNearbyProfile(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findRecentProfile(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findProfileByReligion(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
+
+    ProfileResponse findProfileByHobbies(InternalProfileFilterRequest internalProfileFilterRequest, Long memberId);
 
     Member save(Member member);
 
