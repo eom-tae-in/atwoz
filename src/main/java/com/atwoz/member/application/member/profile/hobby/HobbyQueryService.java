25df25d21055dcc6357a7e49d526ebdd36376dff
eom-tae-in <eti0728@daum.net>
2024-08-11 14:30:24 +0900

    feat: 읽기와 쓰기를 분리하여 HobbyService, HobbyQueryService 생성

---
+++ src/main/java/com/atwoz/member/application/member/profile/hobby/HobbyQueryService.java
@@ -0,0 +1,35 @@
+package com.atwoz.member.application.member.profile.hobby;
+
+import com.atwoz.member.application.member.profile.hobby.dto.HobbyResponses;
+import com.atwoz.member.domain.member.profile.Hobby;
+import com.atwoz.member.domain.member.profile.HobbyRepository;
+import com.atwoz.member.exception.exceptions.member.profile.hobby.HobbyNotFoundException;
+import com.atwoz.member.infrastructure.member.profile.hobby.dto.HobbyResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class HobbyQueryService {
+
+    private final HobbyRepository hobbyRepository;
+
+    public HobbyResponse findHobby(final Long hobbyId) {
+        Hobby foundHobby = findMemberHobbyById(hobbyId);
+        return HobbyResponse.from(foundHobby);
+    }
+
+    private Hobby findMemberHobbyById(final Long hobbyId) {
+        return hobbyRepository.findHobbyById(hobbyId)
+                .orElseThrow(HobbyNotFoundException::new);
+    }
+
+    public HobbyResponses findHobbiesWithPaging(final Pageable pageable) {
+        Page<HobbyResponse> hobbyResponses = hobbyRepository.findHobbiesWithPaging(pageable);
+        return HobbyResponses.of(hobbyResponses, pageable);
+    }
+}
