6f7ae7544e1a6fb5f44c21398c1fc941f167cc42
eom-tae-in <eti0728@daum.net>
2024-08-11 14:28:51 +0900

    feat: 취미 정보를 저장하고 관리하는 Hobby 엔티티 클래스 생성

---
+++ src/main/java/com/atwoz/member/domain/member/profile/Hobby.java
@@ -0,0 +1,50 @@
+package com.atwoz.member.domain.member.profile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Hobby {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    public static Hobby createWith(final String name,
+                                   final String code) {
+        return Hobby.builder()
+                .name(name)
+                .code(code)
+                .build();
+    }
+
+    public void update(final String name,
+                       final String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public boolean isSame(final Hobby hobby) {
+        return hobby.equals(this);
+    }
+}
